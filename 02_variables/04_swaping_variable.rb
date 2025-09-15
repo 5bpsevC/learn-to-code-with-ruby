ee8c9566715877ea1f283e25c794b67b0caff62d
5bpsevC <brite6001@gmail.com>
2025-09-15 20:13:45 +0000

    swaping varible

---
+++ 02_variables/04_swaping_variable.rb
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+
+puts a, b   # Salida:
+            # 1
+            # 2
+
+# Intercambiando los valores de a y b usando asignación múltiple
+a, b = b, a
+
+puts a, b   # Salida:
+            # 2
+            # 1
