32640068f8ded5a889d12300b26814b586739db8
5bpsevC <brite6001@gmail.com>
2025-09-15 19:53:14 +0000

    the p method

---
+++ 01_getting_started/04_the_p_method.rb
@@ -0,0 +1,27 @@
+# puts imprime el contenido como una cadena, y añade un salto de línea al final
+puts "Steven Seagal"  # Salida: Steven Seagal
+
+# p muestra una representación *más detallada* del objeto, útil para depuración
+p "Steven Seagal"     # Salida: "Steven Seagal" (con comillas)
+
+# puts convierte el número a cadena y lo imprime con salto de línea
+puts 5                # Salida: 5
+
+# puts imprime la cadena "5", se verá igual que el número anterior
+puts "5"              # Salida: 5
+
+# p imprime el número tal cual, sin formato especial
+p 5                   # Salida: 5
+
+# p imprime la cadena con comillas
+p "5"                 # Salida: "5"
+
+# puts interpreta \n como salto de línea
+puts "Hi there\nline break"
+# Salida:
+# Hi there
+# line break
+
+# p muestra la cadena literalmente, incluyendo los caracteres de escape
+p "Hi there\nline break"
+# Salida: "Hi there\nline break"
