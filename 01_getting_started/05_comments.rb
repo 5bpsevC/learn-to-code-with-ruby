cbe325111d2f8297bfe0cf738359cb4dc32142b3
5bpsevC <brite6001@gmail.com>
2025-09-15 19:53:22 +0000

    comments

---
+++ 01_getting_started/05_comments.rb
@@ -0,0 +1,8 @@
+# Este es un comentario, no se ejecuta
+# Los comentarios sirven para explicar el código
+
+puts 1 + 1       # Imprime 2 en la consola
+
+puts 2 + 2       # Imprime 4 – El comentario al final no afecta el código
+
+puts 3 + 3       # Imprime 6
