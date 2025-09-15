b88edf13ba13e24260465af7c6c48a2a922cb11c
5bpsevC <brite6001@gmail.com>
2025-09-15 20:20:54 +0000

    intro object methods

---
+++ 03_object_methods/01_intro.rb
@@ -0,0 +1,14 @@
+# Imprime la longitud (cantidad de caracteres) de la cadena "hello world"
+puts "hello world".length      # => 11
+
+# Convierte la cadena "hello world" a mayúsculas
+puts "hello world".upcase      # => "HELLO WORLD"
+
+# Convierte la cadena "TOTALLY DIFFERENT" a minúsculas
+puts "TOTALLY DIFFERENT".downcase   # => "totally different"
+
+# Asigna la cadena a una variable llamada expression
+expression = "TOTALLY DIFFERENT"
+
+# Convierte el contenido de la variable expression a minúsculas y lo imprime
+puts expression.downcase       # => "totally different"
