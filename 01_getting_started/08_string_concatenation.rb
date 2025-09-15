271fbcc19d9f02b201f90d0b39e2f5bdf415ddaf
5bpsevC <brite6001@gmail.com>
2025-09-15 20:02:47 +0000

    string concatenation

--- 01_getting_started/08_string_concatenation.rb
+++ 01_getting_started/08_string_concatenation.rb
@@ -6,3 +6,7 @@ puts "race" + "car"   # Salida: racecar
 
 # Concatenación de cadenas que parecen números
 puts "4" + "3"        # Salida: 43
+
+puts 4 + "3"   # ❌ Error
+# TypeError: String can't be coerced into Integer
+# Porque Ruby no convierte automáticamente entre tipos para operaciones mixtas.
