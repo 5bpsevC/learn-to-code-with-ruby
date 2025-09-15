222013a6b9d510bfe6183e3512b44fce27b9d0ad
5bpsevC <brite6001@gmail.com>
2025-09-15 19:49:57 +0000

    the print method

---
+++ 01_getting_started/03_the_print_method.rb
@@ -0,0 +1,8 @@
+# Imprime el texto sin agregar un salto de línea al final
+print "This random nonsense "
+
+# Imprime otro texto que continuará en la misma línea que el anterior
+print "will continue on the same line"
+
+# A diferencia de puts, el método print no agrega un salto de línea (\n) al final, por lo tanto, ambas cadenas se imprimen en la misma línea.
+# Si usaras puts en lugar de print, cada línea aparecería en una línea separada.
