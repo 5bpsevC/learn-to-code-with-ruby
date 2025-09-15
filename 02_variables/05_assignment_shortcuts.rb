ee4733c49727bb5d46ba598c13f9c6a858e46193
5bpsevC <brite6001@gmail.com>
2025-09-15 20:17:30 +0000

    Assignment shortcuts

---
+++ 02_variables/05_assignment_shortcuts.rb
@@ -0,0 +1,31 @@
+a = 10
+# Forma larga:
+a = a + 5
+
+# Forma corta con operador +=
+a += 7
+puts a   # Salida: 17
+
+b = 100
+# Forma larga:
+# b = b - 40
+
+# Forma corta con operador -=
+b -= 40
+puts b   # Salida: 60
+
+c = 3
+# Forma larga:
+# c = c * 4
+
+# Forma corta con operador *=
+c *= 4
+puts c   # Salida: 12
+
+name = "Boris"
+# Forma larga:
+# name = name + " The Great"
+
+# Forma corta con operador += (concatena cadenas)
+name += " The Great"
+puts name   # Salida: Boris The Great
