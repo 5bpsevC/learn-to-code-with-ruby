a46e6dd9a5142e9ea9a80693b35a796cd18f5a08
5bpsevC <brite6001@gmail.com>
2025-09-15 19:56:58 +0000

    numbers

---
+++ 01_getting_started/06_integers_floating_points_nums.rb
@@ -0,0 +1,16 @@
+# Números enteros positivos, cero y negativos
+puts 100            # Imprime 100
+puts 0              # Imprime 0
+puts (-837 )          # Imprime -837
+
+# Puedes usar guiones bajos para mejorar la legibilidad de números grandes
+puts 1_000          # Imprime 1000
+puts 9_999_999_999_999_999  # Imprime 9999999999999999
+
+puts                # Línea en blanco, imprime un salto de línea
+
+# Números decimales (float)
+puts 3.14           # Imprime 3.14
+puts 0.50           # Imprime 0.5 (Ruby elimina ceros innecesarios al final)
+puts( -10.99  )       # Imprime -10.99
+puts (-0.93 )         # Imprime -0.93
