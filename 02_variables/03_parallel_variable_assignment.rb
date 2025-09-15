25434854329ad8f1a9dd702cf3bc7c4ba55b2d3b
5bpsevC <brite6001@gmail.com>
2025-09-15 20:13:33 +0000

    parallel variable assignment

---
+++ 02_variables/03_parallel_variable_assignment.rb
@@ -0,0 +1,19 @@
+# Asignación separada
+a = 10
+b = 20
+c = 30
+
+puts a, b, c
+# Salida:
+# 10
+# 20
+# 30
+
+# Asignación múltiple en una línea (desestructuración)
+a, b, c = 10, 20, 30
+
+puts a, b, c
+# Salida:
+# 10
+# 20
+# 30
