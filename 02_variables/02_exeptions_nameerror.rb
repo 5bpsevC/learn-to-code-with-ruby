b5880bd23562b3b01e670db0e458d8587ad25c58
5bpsevC <brite6001@gmail.com>
2025-09-15 20:10:34 +0000

    NameError

---
+++ 02_variables/02_exeptions_nameerror.rb
@@ -0,0 +1,11 @@
+favorite_food = "Sushi"
+
+puts favorite_food 
+
+# Un NameError ocurre en Ruby cuando el intérprete intenta usar una variable o método que no ha sido definido
+puts city
+# undefined local variable or method 'city' for main (NameError)
+
+name = "Alice"
+puts nmae
+#  undefined local variable or method 'nmae' for main (NameError)
