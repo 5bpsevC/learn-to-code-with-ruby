4441ac1dd8d7afa2caf21b8b8837532f7d5b37b3
5bpsevC <brite6001@gmail.com>
2025-09-15 20:01:40 +0000

    string concatenation

---
+++ 01_getting_started/08_string_concatenation.rb
@@ -0,0 +1,8 @@
+# Suma de números enteros
+puts 4 + 3            # Salida: 7
+
+# Concatenación de cadenas de texto
+puts "race" + "car"   # Salida: racecar
+
+# Concatenación de cadenas que parecen números
+puts "4" + "3"        # Salida: 43
