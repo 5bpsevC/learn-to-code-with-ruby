a93c16dd4d874a9e9ee765882e5cc44afea101e9
5bpsevC <brite6001@gmail.com>
2025-10-01 20:00:56 +0000

    conditionals

---
+++ 07_conditionals/06_multiple_conditions.rb
@@ -0,0 +1,17 @@
+# Pedimos al usuario que escriba su nombre de usuario
+puts "Please enter username"
+username = gets.chomp  # Guardamos lo que escribe el usuario (sin salto de línea)
+
+# Pedimos al usuario que escriba su contraseña
+puts "Please enter password"
+password = gets.chomp  # Guardamos la contraseña ingresada
+
+# Condición combinada con AND (&&):
+# Ambas condiciones deben ser verdaderas para entrar al if:
+# 1) username debe ser "rubydev1"
+# 2) password debe ser "topsecret"
+if username == "rubydev1" && password == "topsecret"
+  puts "Congrats, you've logged in!"  # Se ejecuta solo si ambas condiciones son ciertas
+else
+  puts "Incorrect, no access for you!"  # Si falla cualquiera de las dos condiciones
+end
